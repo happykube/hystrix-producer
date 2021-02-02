708dcde8c4b258aa5a00e33fdf8c307ed6b2cd6a
happycloudpak <happycloudpak@gmail.com>
2021-02-02 20:26:45 +0900

    add log

--- src/main/java/com/springcloud/Controller.java
+++ src/main/java/com/springcloud/Controller.java
@@ -3,6 +3,8 @@ package com.springcloud;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class Controller {
+	private final Logger log = LoggerFactory.getLogger(getClass());
+	
 	@GetMapping("/coffees/{param}")
 	public List<String> getConffees(@PathVariable String param) {
+		log.info("### Received: /coffees/"+param);
+		
 		if(!"pass".equalsIgnoreCase(param)) {
 			try {
 				Thread.sleep(1000);
 			} catch(Exception e) {}
 		}
 		
-		return Arrays.asList("Americano", "Latte", "Mocha");
+		List<String> list = Arrays.asList("Americano", "Latte", "Mocha");
+		log.info("### Sent: "+list.toString());
+		
+		return list;
 	}
 }
